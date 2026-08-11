6fe7b1f1d4809d92246f2a7e78492325fb5b5653
trostsalman-sketch <trostsalman@gmail.com>
2026-08-11 19:36:30 +0500

    Create logger.py

---
+++ bot/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+def setup_logger(name: str = __name__) -> logging.Logger:
+    """Настройка логгера"""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    handler.setFormatter(formatter)
+    
+    logger.addHandler(handler)
+    
+    return logger
