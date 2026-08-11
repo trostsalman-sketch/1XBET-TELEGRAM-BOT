111ec6a1eda7a9019625e1c3bc8ace68b158f5ff
trostsalman-sketch <trostsalman@gmail.com>
2026-08-11 21:12:00 +0500

    Update __init__.py

--- bot/handlers/user/__init__.py
+++ bot/handlers/user/__init__.py
@@ -1,1 +1,9 @@
-from . import user, admin
+from aiogram import Router
+from . import start, bet, profile, history
+
+router = Router()
+
+router.include_router(start.router)
+router.include_router(bet.router)
+router.include_router(profile.router)
+router.include_router(history.router)
